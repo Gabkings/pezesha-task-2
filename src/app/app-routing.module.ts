52a3c2c7d91f146b88dcc1ec3a55d6fd4a856a3f
dev <dev@localhost.localdomain>
2020-02-14 15:13:50 +0300

    Configure routes for the Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { ResultComponent } from './result/result.component';
+import { CompanyComponent } from './company/company.component';
+import { PersonalComponent } from './personal/personal.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { DocumentComponent } from './document/document.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //1st route
+    {path: 'personal', component: PersonalComponent},
+    //2nd route
+    {path: 'company', component: CompanyComponent},
+    //3rd route
+    {path: 'document', component: DocumentComponent},
+    //4th route
+    {path: 'result', component: ResultComponent},
+    //5th route
+    {path: '', redirectTo: '/personal', pathMatch: 'full'},
+    //6th route
+    {path: '**', component:PersonalComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes, {useHash:true})],
